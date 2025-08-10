9cadd2032bf5eafac2b93b805138269c72f31936
griffinjoshs <leveloutusa@gmail.com>
2025-08-10 01:56:43 -0400

    inital commit

--- src/Sections/WebsiteTypes.jsx
+++ src/Sections/WebsiteTypes.jsx
@@ -207,11 +207,23 @@ const WebsiteTypes = () => {
   }, []);
 
   return (
-    <section className="outer-section bg-secondary relative">
-      <div className="section-dim-border"></div>
-      <div className="inner-section">
-        <div className="text-section">
-          <div className="border-title">Website Types</div>
+    <>
+      <style jsx>{`
+        @media (max-width: 1024px) {
+          [data-accordion-container] {
+            scroll-behavior: smooth;
+          }
+
+          [data-accordion-item] {
+            transition: all 0.1s ease !important; 
+          }
+        }  
+   `}</style>
+      <section className="outer-section bg-secondary relative">
+        <div className="section-dim-border"></div>
+        <div className="inner-section">
+          <div className="text-section">
+            <div className="border-title">Website Types</div>
           <h2 className="h2 mb-6">
             Websites We <span className="text-accent-heading">Build</span>
           </h2>
@@ -331,6 +343,7 @@ const WebsiteTypes = () => {
         </div>
       </div>
     </section>
+    </>
   );
 };
 
