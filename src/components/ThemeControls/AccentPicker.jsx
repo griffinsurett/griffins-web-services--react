2d6662f2a330b88c352957e65c7270057d220f70
griffinjoshs <leveloutusa@gmail.com>
2025-08-25 00:07:38 -0400

    taglines

--- src/components/ThemeControls/AccentPicker.jsx
+++ src/components/ThemeControls/AccentPicker.jsx
@@ -28,7 +28,7 @@ export default function AccentPicker() {
         aria-label="Pick accent color"
         className="faded-bg"
       >
-        <svg width="22" height="22" viewBox="0 0 100 100">
+        <svg className="w-[18px] h-[18px] lg:w-[22px] lg:h-[22px]" viewBox="0 0 100 100">
           {" "}
           <path
             d="M50 10C50 10 25 35 25 55C25 70.464 37.536 83 50 83C62.464 83 75 70.464 75 55C75 35 50 10 50 10Z"
